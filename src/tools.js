93d884fa72dacd932ad4a0fbc0858ca4c29ba8dd
Jinghua Li <jinhli@thoughtworks.com>
2020-02-23 18:00:39 +0800

    Implement that parse input string to map

--- src/tools.js
+++ src/tools.js
@@ -1,5 +1,12 @@
 function parseInputToMap(selectedItems) {
   // need to be implement
+  return new Map(
+    selectedItems.map((item) => {
+      let matchs = item.split(" x ");
+      let count = parseInt(matchs[1]);
+      return [matchs[0], count];
+    })
+  );
 }
 
 module.exports = { parseInputToMap };
