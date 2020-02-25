664ae67a67c85d595bfd33f4be3d54da95b1bcff
Jinghua Li <jinhli@thoughtworks.com>
2020-02-25 20:34:04 +0800

    Fix parse input test fucntion

--- spec/tools-spec.js
+++ spec/tools-spec.js
@@ -2,7 +2,7 @@ const tools = require("../src/tools.js");
 
 describe("Take out food", function() {
   it("should return a map of items when parse input string", () => {
-    let inputs = ["ITEM0001 x 1", "ITEM0013 x 2", "ITEM0022 x 1"];
+    let inputs = "ITEM0001 x 1,ITEM0013 x 2,ITEM0022 x 1";
     let result = tools.parseInputToMap(inputs);
     expect(result).toEqual(
       new Map([
