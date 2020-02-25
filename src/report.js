1061f33473dd9f7be9d39790ae03c7966a22b1c9
Jinghua Li <jinhli@thoughtworks.com>
2020-02-25 10:24:59 +0800

    Complete normal order print

--- src/report.js
+++ src/report.js
@@ -3,8 +3,31 @@ class Report {
     this.order = order;
   }
 
+  getItemDetailStr() {
+    return this.order.itemDetails
+      .map(
+        (item) =>
+          `` +
+          item.name +
+          " x " +
+          item.count +
+          " = " +
+          item.count * item.price +
+          "元" +
+          `
+      `
+      )
+      .join("")
+      .trim();
+  }
+
   print() {
-    // Need to be implement
+    return `
+      ============= 订餐明细 =============
+      ${this.getItemDetailStr()}
+      -----------------------------------
+      总计：${this.order.calTotalPrice()}元
+      ===================================`.trim();
   }
 }
 
